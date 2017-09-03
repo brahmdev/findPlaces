f8dd16570f891b5d88e51d865355927df609d96b
brahmdev <pandey.dev25@gmail.com>
2017-09-03 09:51:49 +0200

    property file & logging added

--- server/src/main/java/org/smartbear/services/rest/FindPlacesService.java
+++ server/src/main/java/org/smartbear/services/rest/FindPlacesService.java
@@ -1,15 +1,20 @@
 package org.smartbear.services.rest;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.json.JSONException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.smartbear.modal.ElevationSearchModal;
 import org.smartbear.modal.PlaceSearchModal;
 import org.smartbear.modal.ResponseModal;
 import org.smartbear.util.SBUtil;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -24,41 +29,75 @@ import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+/**
+ * 
+ * @author KalpDev
+ *
+ */
+
 @RestController
 @RequestMapping(value = "/service", produces = MediaType.APPLICATION_JSON_VALUE)
+@PropertySource("classpath:config.properties")
 public class FindPlacesService {
 
+	private static final Logger logger = LoggerFactory.getLogger(FindPlacesService.class);
+	
+	@Value("${SEARCH.API.KEY}")
+	private String SEARCH_API_KEY;
+	
+	@Value("${ELEVATION.API.KEY}")
+	private String ELEVATION_API_KEY;
+	/**
+	 * 
+	 * @param location
+	 * @param radius
+	 * @param type
+	 * @param keyword
+	 * @return
+	 * @throws JSONException
+	 * @throws JsonParseException
+	 * @throws JsonMappingException
+	 * @throws IOException
+	 */
 	@RequestMapping(method = RequestMethod.POST, value = "/findPlaces/{location}/{radius}")
-	public ResponseEntity<List<ResponseModal>> getPlaceForMe(@PathVariable String location, @PathVariable String radius, @RequestParam(value="type") String type, @RequestParam(value="keyword") String keyword) throws JSONException, JsonParseException, JsonMappingException, IOException {
+	public ResponseEntity<List<ResponseModal>> getPlaceForMe(@PathVariable String location, @PathVariable String radius, @RequestParam(value="type") String type, @RequestParam(value="keyword") String keyword) {
 		RestTemplate restTemplate = new RestTemplate();
 		Map<String, String> vars = new HashMap<String, String>();
 		ObjectMapper mapper = new ObjectMapper();
+		List<ResponseModal> responseModalList = new ArrayList<ResponseModal>();
 
 		vars.put("location", location);
 		vars.put("radius", radius);
 		vars.put("type", type);
 		vars.put("keyword", keyword);
-		vars.put("key", "AIzaSyD9GhIz_uwO_xLKFoffiwmF0RIL05WLckM");
+		vars.put("key", SEARCH_API_KEY);
+		
+		try {
 		String resultPlaceSearch = restTemplate
 		.getForObject("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={location}&radius={radius}&type={type}&keyword={keyword}&key={key}",
 		String.class, vars);
 		
-		System.out.println(resultPlaceSearch);
 		PlaceSearchModal placeSearchModal = mapper.readValue(resultPlaceSearch, PlaceSearchModal.class);
 		
-		
-		vars.put("key", "AIzaSyD3EHtHKQxOAxxtoVIGbc0aSb4DuRYV4QY");
+		vars.put("key", ELEVATION_API_KEY);
 		String resultElevationSearch = restTemplate
 		.getForObject("https://maps.googleapis.com/maps/api/elevation/json?locations={location}&key={key}",
 		String.class, vars);
 		
 		ElevationSearchModal elevationSearchModal = mapper.readValue(resultElevationSearch, ElevationSearchModal.class);
-		System.out.println(resultElevationSearch);
 		
-		List<ResponseModal> responseModalList = SBUtil.getResponseModalList(placeSearchModal, elevationSearchModal);
+		responseModalList = SBUtil.getResponseModalList(placeSearchModal, elevationSearchModal);
 
-		System.out.println(responseModalList.size());
-		
+		logger.info("Total " + responseModalList.size() + " records mapped sucessfully!");
+		} catch(JsonParseException ex) {
+			logger.error("JSON was not parsed because of : " + ex.getMessage());
+		} catch(JsonMappingException ex) {
+			logger.error("Following excaption came while mapping the JSON : " + ex.getMessage());
+		} catch(IOException ex) {
+			logger.error(ex.getMessage());
+		} catch(Exception ex) {
+			logger.error(ex.getMessage());
+		} 
 		return new ResponseEntity<>(responseModalList, HttpStatus.OK);
 	}
 	
